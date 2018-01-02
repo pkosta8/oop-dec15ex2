91421902edaa6d9228c9d5ab11bc0f834fafd288
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-01 19:33:41 -0500

    ex4.2

--- ex4.rb
+++ ex4.rb
@@ -2,8 +2,31 @@
 
 class Paperboy
 
-  def initialize(name)
-    @name = name
-    @experience = 0
-    @earnings = 0
-  end
+def initialize(name)
+@name = name
+@experience = 0
+@earnings = 0
+end
+
+
+
+
+
+def quota
+return 50 + @experience / 2
+end
+
+
+def deliver(start_address, end_address)
+total_houses = end_address - start_address + 1
+latest_payment = calculate_pay(number_of_houses)
+@earnings += latest_payment
+@experience += total_houses
+return latest_payment
+end
+
+
+
+def report
+return "I'm #{@name}, I've delivered #{@experience} papers and I've earned #{@earnings} so far!"
+end
